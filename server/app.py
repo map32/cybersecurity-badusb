4c66552148ff2a78330514e10f6d4e0d43a8bfbb
map32 <shin3@cooper.edu>
2023-12-12 07:00:38 -0500

    a

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request, jsonify
 import json
 from datetime import datetime
-
+data = {'data':[]}
 app = Flask(__name__)
 def create_timestamp():
     now = datetime.now()
